6d6a18a7e1062ba8d0ff2e64d5c857c5c0b8c504
Drobysheva Olga <drobysheva.olya@mail.ru>
2023-10-02 23:43:32 +0300

    Add apps cart, orders, products, users

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market'
+    'products',
+    'users',
+    'orders',
+    'cart',
+    'slugify',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
